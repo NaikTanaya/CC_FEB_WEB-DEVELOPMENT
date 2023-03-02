4ec6d850c70964cea67ced50562d2acba816b0b2
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2023-03-02 22:28:45 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,41 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title></title>
+     <style>
+        #footer{
+            background-color: #000;
+            color: #fff;
+            padding: 20px 0;
+            bottom: 0;
+            position: absolute;
+            width: 100%;
+        }
+     </style>
+ </head>
+
+ <body>
+     <footer id="footer" class="midnight-blue wow fadeInDown">
+         <div class="container">
+             <div class="row">
+                 <div class="col-sm-6 wow fadeInDown">
+                    <a target="_blank" href="#" title="#">Pet Adoption Portal</a>. All Rights Reserved. &copy; 2023 
+                 </div>
+                 <div class="col-sm-6">
+                     <ul class="pull-right wow fadeInDown">
+                         <li class="wow fadeInDown"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+
+                         <li class="wow fadeInDown"><a href="contacts.php"><i class="fa fa-phone"></i> Contacts</a></li>
+                         <li class="wow fadeInDown"><a href="#loginModal" data-toggle="modal" data-target="#loginModal"><i class="fa fa-lock"></i> Admin</a></li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+     </footer><!--/#footer-->
+ </body>
+
+ </html>
